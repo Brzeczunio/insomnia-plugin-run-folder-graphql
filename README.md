47a51827b30983df104be65181b11526aead9ffe
Brzeczunio <33999336+Brzeczunio@users.noreply.github.com>
2020-05-29 09:59:20 +0200

    Add plugin for graphql

---
+++ README.md
@@ -0,0 +1,6 @@
+# insomnia-plugin-run-folder-graphql
+This plugin adds an option to each folder's drop-down menu so that all requests within can be ran automatically in order and show results specific for GraphQL.
+
+The original code was taken from https://www.npmjs.com/package/insomnia-plugin-run-folder and the author is [Daniel Castro](github:LunaticoCR).
+
+This plugin is published in [npm](https://www.npmjs.com/package/insomnia-plugin-run-folder-graphql). To use it you can install it from your [insomnia.rest](https://insomnia.rest/) client by going into **Preferences -> Plugins** and entering the package name ```insomnia-plugin-run-folder-graphql``` then click "Install Plugin".
---
+++ run-folder-graphql.js
@@ -0,0 +1,90 @@
+module.exports.requestGroupActions = [
+  {
+    label: 'Send All Requests',
+    action: async (context, data) => {
+      const { requests } = data;
+
+      var count = 0;
+      let results = [];
+      results.push(`<tr>
+                      <th>Request</th>
+                      <th>Status Code</th>
+					  <th>Content Code</th>
+					  <th>Content</th>
+                      <th>Time</th>
+                      <th>Bytes</th>
+                    </tr>`);
+      for (const request of requests) {
+        const response = await context.network.sendRequest(request);
+		
+		const fs = require('fs');
+		var responseData = JSON.parse(fs.readFileSync(response.bodyPath, function (exists){}));
+		
+        var color = getStatusCodeColor(response.statusCode);
+        var time = millisecToHumanReadable(response.elapsedTime);
+		var colorTime = getTimeColor(time);
+		
+		if(responseData.errors)
+		{
+			results.push(`<tr>
+							<td id="td_right">[${request.method}] </br> ${request.name}</td>
+							<td id="td_right"><font color="${color}">${response.statusCode} ${response.statusMessage}</font></td>
+							<td id="td_right"><font color="#D8696F">${responseData.errors[0].code}</td>
+							<td id="td_right"><font color="#D8696F">${responseData.errors[0].message}</td>
+							<td id="td_right"><font color="${colorTime}">${time}</td>
+							<td id="td_right">${response.bytesRead}</td>
+						  </tr>`);		
+			count = count + 1;
+		} else
+        results.push(`<tr>
+                        <td id="td_right">[${request.method}] </br> ${request.name}</td>
+                        <td id="td_right"><font color="${color}">${response.statusCode} ${response.statusMessage}</font></td>
+						<td id="td_right"></td>
+						<td id="td_right"></td>
+                        <td id="td_right"><font color="${colorTime}">${time}</td>
+                        <td id="td_right">${response.bytesRead}</td>
+                      </tr>`);
+      }
+	  
+	results.push(`<div>Udało się ${requests.length - count} z ${requests.length}</div>`);
+
+      const css = `table { margin: 0 auto 0 auto; }
+                   th, .label--small, label > small { text-align: center; padding: 10px 10px 10px 15px !important; padding-bottom: none !important; font-size: 15px !important; border: 1px solid #4B505C; }
+                   td { border: 1px solid #4B505C; bgcolor="#ffffff"; }
+                   #td_left { text-align: left; padding: 3px 40px 3px 10px; }
+                   #td_right { text-align: center; padding: 3px 3px 3px 3px; }`
+      const html = `<html><head><style>${css}</style></head><body><table bgcolor="#ffffff">${results.join('\n')}</table></body></html>`;
+
+      context.app.showGenericModalDialog('Results', { html });
+    },
+  },
+];
+
+function getStatusCodeColor(statusCode) {
+  if (statusCode.toString().startsWith("2")) {
+    return "#8AB46C";
+  } else if (statusCode.toString().startsWith("4")) {
+    return "#D19A66";
+  } else if (statusCode.toString().startsWith("5")) {
+    return "#D8696F";
+  }
+}
+
+function getTimeColor(time) {
+  if (parseInt(time) > 10000) {
+    return "#D8696F";
+  } else if (parseInt(time) < 200) {
+    return "#8AB46C";
+  } else
+	return "#D19A6";
+}
+
+function millisecToHumanReadable(millisec) {
+  var seconds = (millisec / 1000).toFixed(2);
+
+  if (millisec < 1000) {
+    return millisec.toFixed(0) + " ms";
+  } else if (seconds < 60) {
+    return seconds + " s";
+  }
+}
